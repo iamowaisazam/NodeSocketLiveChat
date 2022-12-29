74eaf493be828101e3ec3f95b2afb6fd103f3037
Owais Azam <40581073+iamowaisazam@users.noreply.github.com>
2022-12-29 10:24:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,9 @@ const app = require('express')();
 const { v4 } = require('uuid');
 
 app.get('/', (req, res) => {
-  
-    const path = `/api/item/${v4()}`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+    
+    console.log('hello');
+     res.send("Hello world!");
 });
 
 console.log('App is running');
