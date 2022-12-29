02cef644ac08e3ef7a1c9571f8b6c8ad33f248ee
Owais Azam <40581073+iamowaisazam@users.noreply.github.com>
2022-12-29 10:08:41 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
 
-const PORT = process.env.PORT || 3000
+// const http = require('http').createServer(app);
+// const PORT = process.env.PORT || 3000
+// http.listen(PORT,() => {
+//  console.log('Server Is Running '+PORT)
+// });
 
-http.listen(PORT,() => {
- console.log('Server Is Running '+PORT)
-
-});
+ console.log('Server Is Running')
 
 app.use(express.static(__dirname + '/public'))
 
@@ -15,17 +15,17 @@ app.get('/',(req,res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
-const io = require('socket.io')(http)
+// const io = require('socket.io')(http)
 
-io.on('connection',(socket) => {
-    console.log('Connected..');
+// io.on('connection',(socket) => {
+//     console.log('Connected..');
 
-    socket.on('message',(msg) => {
-    //  console.log(msg);
-    // socket.broadcast.emit('message',msg);
-     io.emit('message', msg);
-    });
+//     socket.on('message',(msg) => {
+//     //  console.log(msg);
+//     // socket.broadcast.emit('message',msg);
+//      io.emit('message', msg);
+//     });
 
   
-});
+// });
 
